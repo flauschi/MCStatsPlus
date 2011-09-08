fa77884351bbef3c3051ddb36753e2a014191608
Robert Rieß <flauschsheep@googlemail.com>
2011-09-08 19:56:14 +0200

    changed config file

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ define('MCSP_TYPE', 'STANDALONE');
 
 define('MCSP_DB_TYPE', 'mysql');
 define('MCSP_DB_HOST', 'localhost');
-define('MCSP_DB_NAME', 'dbname');
-define('MCSP_DB_USER', 'user');
+define('MCSP_DB_NAME', 'database');
+define('MCSP_DB_USER', 'username');
 define('MCSP_DB_PASSWORD', 'password');
 define('MCSP_DB_CHARSET', 'utf8');
 
